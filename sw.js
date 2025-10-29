ad9ba302d9bf73ff858390873e6b894948d41abb
R3DRUMVNE <simolkin.pavel@ya.ru>
2025-10-29 21:52:10 +0300

    + serviceWorker

---
+++ sw.js
@@ -0,0 +1,91 @@
+const staticCacheName = "static_cache_v0"
+
+const static_assets = [
+    ".",
+    "./index.html",
+    "./assets/favicon.svg",
+    "./assets/daicamlock/camlockFather_ExternalThread.png",
+    "./assets/daicamlock/camlockFather_HoseFitting.png",
+    "./assets/daicamlock/camlockFather_InternalThread.png",
+    "./assets/daicamlock/camlockFather_Stub.png",
+    "./assets/daicamlock/camlockMother_ExternalThread.png",
+    "./assets/daicamlock/camlockMother_HoseFitting.png",
+    "./assets/daicamlock/camlockMother_InternalThread.png",
+    "./assets/daicamlock/camlockMother_Stub.png",
+    "./assets/share/tg.png",
+    "./assets/share/vk.png",
+    "./assets/shops/lp.png",
+    "./assets/shops/ozon.png",
+    "./assets/shops/vi.png",
+    "./assets/shops/wb.png",
+    "./assets/shops/ym.png",
+    "./assets/bugReport.svg",
+    "./assets/daicamlock.svg",
+    "./assets/download.svg",
+    "./assets/gidrohod.svg",
+    "./assets/menuButton.svg",
+    "./assets/moduleHint.svg",
+    "./assets/prokachaika.svg",
+    "./assets/schetovod.svg",
+    "./assets/settingsInfo.svg",
+    "./assets/share.svg",
+    "./assets/tehpas.svg",
+    "./objects/camlockImageSizesList.json",
+    "./objects/camlockTypeList.json",
+    "./objects/daicamlockStringList.json",
+    "./objects/mainStringList.json",
+    "./objects/menuMap.json",
+    "./objects/moduleHintStringList.json",
+    "./objects/newPumps.json",
+    "./objects/passportMap.json",
+    "./objects/prokachaikaStringList.json",
+    "./objects/pumpList.json",
+    "./objects/schetovodStringList.json",
+    "./objects/tehpasStringList.json",
+    "./objects/themesList.json",
+    "./scripts/moduleScripts/bufferModule.js",
+    "./scripts/moduleScripts/otherModules.js",
+    "./scripts/app.js",
+    "./scripts/daicamlock.js",
+    "./scripts/main.js",
+    "./scripts/prokachaika.js",
+    "./scripts/schetovod.js",
+    "./scripts/tehpas.js",
+    "./styles/daicamlock.css",
+    "./styles/daicamlockAdaptive.css",
+    "./styles/dsPrint.css",
+    "./styles/main.css",
+    "./styles/mainAdaptive.css",
+    "./styles/osPrint.css",
+    "./styles/prokachaika.css",
+    "./styles/prokachaikaAdaptive.css",
+    "./styles/schetovod.css",
+    "./styles/schetovodAdaptive.css",
+    "./styles/tehpas.css",
+    "./styles/tehpasAdaptive.css",
+    "./styles/tehpasPrint.css",
+];
+
+self.addEventListener('install', async () => {
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(static_assets);
+    console.log('Service worker has been installed');
+});
+
+self.addEventListener('activate', async () => {
+    const cachesKeys = await caches.keys();
+    const checkKeys = cachesKeys.map(async key => {
+        if (staticCacheName !== key) {
+            await caches.delete(key);
+        }
+    });
+    await Promise.all(checkKeys);
+    console.log('Service worker has been activated');
+});
+
+self.addEventListener('fetch', event => {
+    console.log(`Trying to fetch ${event.request.url}`);
+    event.respondWith(caches.match(event.request).then(cachedResponse => {
+        return cachedResponse || fetch(event.request);
+    }));
+});
