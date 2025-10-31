48f6fee9d960c3138cb210084a6c9f721ce92336
R3DRUMVNE <simolkin.pavel@ya.ru>
2025-10-31 21:22:06 +0300

    - изменение метода определения режима онлайн в багрепорте - заменена ссылка кнопки "Загрузить на устройство"

--- scripts/main.js
+++ scripts/main.js
@@ -149,7 +149,7 @@ function showSettingsInfo() {
     let downloadButton = createElement(itemsContainer, "div", "id=downloadButton / class=iconButton", mainStringList);
     createElement(downloadButton, "img", "id=downloadButtonImg / src=./assets/download.svg");
     downloadButton.onclick = function () {
-        window.open('https://github.com/R3DRUMVNE/DrillMate/releases/', '_blank');
+        window.open("https://github.com/R3DRUMVNE/DrillMate/wiki/%F0%9F%93%B2-%D0%97%D0%B0%D0%B3%D1%80%D1%83%D0%B7%D0%B8%D1%82%D1%8C-%D0%BD%D0%B0-%D1%83%D1%81%D1%82%D1%80%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%BE-%7C-%D0%9E%D1%84%D0%BB%D0%B0%D0%B9%D0%BD-%D1%80%D0%B5%D0%B6%D0%B8%D0%BC", '_blank');
     }
 
     let closeButton = createElement(itemsContainer, "button", "id=closeButton", mainStringList);
@@ -191,10 +191,7 @@ function showBugReport() {
 }
 
 function getStatus(){
-    if(window.location.href==="https://r3drumvne.github.io/DrillMate/" || window.location.href==="https://r3drumvne.github.io/DrillMate/index.html"){
-        return "Онлайн";
-    }
-    return "Офлайн";
+    return navigator.onLine ?  "Онлайн" : "Офлайн";
 }
 
 function getOS() {
